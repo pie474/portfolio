3931198b99e63bad396e492d09eb72738c104ca7
pie474 <chinmaymurthy474@gmail.com>
2025-05-30 17:56:19 -0700

    conductor and portfolio projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,11 +5,26 @@ export default function Projects() {
     <div>
       <div className="grid md:grid-cols-2 gap-4">
         <Card
+          title="Portfolio Website"
+          description="I built this website from scratch because I wanted to learn React!"
+          tags={['React', 'Next', 'Tailwind', 'Typescript']}
+          link="https://github.com/pie474/portfolio"
+          date="May 2025"
+        />
+        <Card
           title="Two Wheel-Legged Balancing Robot"
           description="Designed LQR based control algorithm to balance a two-wheel robot with 5-bar active suspension legs. Simulated then deployed to an STM32-based robot."
           tags={['STM32', 'C++', 'Simulink', 'WIP']}
           link="/projects/balstd"
           date="May 2025"
+          icon="/projects/balstd/balstd_icon.jpg"
+        />
+        <Card
+          title="Conductor"
+          description="A CV pipeline that aims to sync MIDI music playback to video feed of a conductor, similar to how a human musician would play their instrument in an ensemble."
+          tags={['Python', 'CV', 'WIP']}
+          link="/projects/balstd"
+          date="Nov 2024"
         />
       </div>
     </div>
