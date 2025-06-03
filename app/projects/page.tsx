4d93141fd77544f16304a9875874de03e3de0b24
pie474 <chinmaymurthy474@gmail.com>
2025-06-02 21:49:04 -0700

    more balstd tags

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -14,7 +14,7 @@ export default function Projects() {
         <Card
           title="Two Wheel-Legged Balancing Robot"
           description="Designed LQR based control algorithm to balance a two-wheel robot with 5-bar active suspension legs. Simulated then deployed to an STM32-based robot."
-          tags={['STM32', 'C++', 'Simulink', 'WIP']}
+          tags={['STM32', 'C++', 'Matlab', 'Simulink', 'WIP']}
           link="/projects/balstd"
           date="May 2025"
           icon="/projects/balstd/balstd_icon.jpg"
