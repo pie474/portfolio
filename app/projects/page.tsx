bd8aae3ab965921109f72a1905a10a52398fce7f
pie474 <chinmaymurthy474@gmail.com>
2025-06-16 15:00:47 -0700

    updated dates

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,18 +5,18 @@ export default function Projects() {
     <div>
       <div className="grid md:grid-cols-2 gap-4">
         <Card
-          title="Portfolio Website"
-          description="I built this website from scratch because I wanted to learn React!"
-          tags={['React', 'Next', 'Tailwind', 'Typescript']}
-          link="https://github.com/pie474/portfolio"
-          date="May 2025"
-        />
-        <Card
           title="Two Wheel-Legged Balancing Robot"
           description="Designed LQR based control algorithm to balance a two-wheel robot with 5-bar active suspension legs. Simulated then deployed to an STM32-based robot."
           tags={['STM32', 'C++', 'Matlab', 'Simulink', 'WIP']}
           link="/projects/balstd"
-          date="May 2025"
+          date="Jun 2025"
+        />
+        <Card
+          title="Portfolio Website"
+          description="I built this website from scratch because I wanted to learn React!"
+          tags={['React', 'Next', 'Tailwind', 'Typescript']}
+          link="https://github.com/pie474/portfolio"
+          date="Jun 2025"
         />
         <Card
           title="Conductor"
