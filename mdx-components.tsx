7332dae46fe8aff4453c8777b32134ee460d98f8
pie474 <chinmaymurthy474@gmail.com>
2025-06-18 15:13:30 -0700

    some mdx component styling

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,18 @@ import type { MDXComponents } from 'mdx/types'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
+        h1: ({ children }) => (
+            <h1 className="text-3xl font-semibold mb-4">{children}</h1>
+        ),
+        h2: ({ children }) => (
+            <h2 className="text-xl font-semibold mb-2">{children}</h2>
+        ),
+        h3: ({ children }) => (
+            <h3 className="font-semibold mb-2">{children}</h3>
+        ),
+        p: ({ children }) => (
+            <p className="text-gray-300">{children}</p>
+        ),
         ...components,
     }
 }
