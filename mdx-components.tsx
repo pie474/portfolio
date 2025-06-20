a7f1890025f11d7111d0b3fa743de48f942a2000
pie474 <chinmaymurthy474@gmail.com>
2025-06-20 03:18:22 -0700

    margins

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,16 +3,16 @@ import type { MDXComponents } from 'mdx/types'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
         h1: ({ children }) => (
-            <h1 className="text-3xl font-semibold mb-4">{children}</h1>
+            <h1 className="text-3xl font-semibold mb-4 mt-8">{children}</h1>
         ),
         h2: ({ children }) => (
-            <h2 className="text-xl font-semibold mb-2">{children}</h2>
+            <h2 className="text-xl font-semibold mb-2 mt-4">{children}</h2>
         ),
         h3: ({ children }) => (
-            <h3 className="font-semibold mb-2">{children}</h3>
+            <h3 className="font-semibold mb-2 mt-4">{children}</h3>
         ),
         p: ({ children }) => (
-            <p className="text-gray-300">{children}</p>
+            <p className="text-gray-300 mt-2">{children}</p>
         ),
         ...components,
     }
