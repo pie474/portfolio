fb5ab2b9efd46e87cf58d58e33dd469722d05e57
pie474 <chinmaymurthy474@gmail.com>
2025-11-05 03:43:54 -0800

    migrate balstd project page

--- components/mdx_layout.tsx
+++ components/mdx_layout.tsx
@@ -1,4 +1,3 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
-    // Create any shared layout or styles here
     return <div className="max-w-3xl mx-auto pt-24">{children}</div>
 }
