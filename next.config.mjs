2722e74ebec085b82bd195574d78e9b40bd1824e
pie474 <chinmaymurthy474@gmail.com>
2026-07-23 20:23:46 -0700

    update stuff in an attempt to fix acorn issue

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,10 @@ import rehypeKatex from 'rehype-katex'
 const nextConfig = {
     // Configure `pageExtensions` to include markdown and MDX files
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-    // Optionally, add any other Next.js config below
 }
 
 const withMDX = createMDX({
     // Add markdown plugins here, as desired
-    extension: /\.(md|mdx)$/,
     options: {
         remarkPlugins: ['remark-math'],
         rehypePlugins: ['rehype-katex'],
